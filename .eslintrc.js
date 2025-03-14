22029b63d5f61ef0f157dd79fd923f9bb5be8801
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2025-03-14 12:55:45 -0500

    Updated react-scripts, fix
    React Version Upgrade:
    Updated React and ReactDOM from v16.13.1 to v17.0.2
    Build Tools Upgrade:
    Updated react-scripts from v0.9.5 to v4.0.3
    Added browserslist configuration required by newer react-scripts
    Node 20 Compatibility:
    Added the OpenSSL legacy provider flag to enable webpack to work with Node 20
    Updated the engines field to specify compatible Node versions
    Fixed Icon Loading:
    Updated SVG imports to use modern ES6 import syntax instead of require()
    This is a key change in how assets are handled between older and newer webpack versions
    ESLint Configuration:
    Created a proper .eslintrc.js file compatible with modern JavaScript and React

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
--- src/icons.js
+++ src/icons.js
@@ -1,34 +1,61 @@
+import clearDay from './icons/clear_day.svg';
+import cloudy from './icons/cloudy.svg';
+import mostlyClearDay from './icons/mostly_clear_day.svg';
+import partlyCloudyDay from './icons/partly_cloudy_day.svg';
+import mostlyCloudy from './icons/mostly_cloudy.svg';
+import fog from './icons/fog.svg';
+import fogLight from './icons/fog_light.svg';
+import drizzle from './icons/drizzle.svg';
+import rain from './icons/rain.svg';
+import rainLight from './icons/rain_light.svg';
+import rainHeavy from './icons/rain_heavy.svg';
+import snow from './icons/snow.svg';
+import flurries from './icons/flurries.svg';
+import snowLight from './icons/snow_light.svg';
+import snowHeavy from './icons/snow_heavy.svg';
+import freezingDrizzle from './icons/freezing_drizzle.svg';
+import freezingRain from './icons/freezing_rain.svg';
+import freezingRainLight from './icons/freezing_rain_light.svg';
+import freezingRainHeavy from './icons/freezing_rain_heavy.svg';
+import icePellets from './icons/ice_pellets.svg';
+import icePelletsHeavy from './icons/ice_pellets_heavy.svg';
+import icePelletsLight from './icons/ice_pellets_light.svg';
+import tstorm from './icons/tstorm.svg';
+import arrow from './icons/icon-arrow-up.svg';
+import sunrise from './icons/sunrise.svg';
+import sunset from './icons/sunset.svg';
+
 //TODO leverage night icons
 const weatherIcons = {
-    1000: require('./icons/clear_day.svg'),
-    1001: require('./icons/cloudy.svg'),
-    1100: require('./icons/mostly_clear_day.svg'),
-    1101: require('./icons/partly_cloudy_day.svg'),
-    1102: require('./icons/mostly_cloudy.svg'),
-    2000: require('./icons/fog.svg'),
-    2100: require('./icons/fog_light.svg'),
+    1000: clearDay,
+    1001: cloudy,
+    1100: mostlyClearDay,
+    1101: partlyCloudyDay,
+    1102: mostlyCloudy,
+    2000: fog,
+    2100: fogLight,
     // 3000: "Light Wind",
     // 3001: "Wind",
     // 3002: "Strong Wind",
-    4000: require('./icons/drizzle.svg'),
-    4001: require('./icons/rain.svg'),
-    4200: require('./icons/rain_light.svg'),
-    4201: require('./icons/rain_heavy.svg'),
-    5000: require('./icons/snow.svg'),
-    5001: require('./icons/flurries.svg'),
-    5100: require('./icons/snow_light.svg'),
-    5101: require('./icons/snow_heavy.svg'),
-    6000: require('./icons/freezing_drizzle.svg'),
-    6001: require('./icons/freezing_rain.svg'),
-    6200: require('./icons/freezing_rain_light.svg'),
-    6201: require('./icons/freezing_rain_heavy.svg'),
-    7000: require('./icons/ice_pellets.svg'),
-    7101: require('./icons/ice_pellets_heavy.svg'),
-    7102: require('./icons/ice_pellets_light.svg'),
-    8000: require('./icons/tstorm.svg'),
-    arrow: require('./icons/icon-arrow-up.svg'),
-    sunrise: require('./icons/sunrise.svg'),
-    sunset: require('./icons/sunset.svg'),
+    4000: drizzle,
+    4001: rain,
+    4200: rainLight,
+    4201: rainHeavy,
+    5000: snow,
+    5001: flurries,
+    5100: snowLight,
+    5101: snowHeavy,
+    6000: freezingDrizzle,
+    6001: freezingRain,
+    6200: freezingRainLight,
+    6201: freezingRainHeavy,
+    7000: icePellets,
+    7101: icePelletsHeavy,
+    7102: icePelletsLight,
+    8000: tstorm,
+    arrow: arrow,
+    sunrise: sunrise,
+    sunset: sunset,
 };
 
 export function getIcon(weatherCode) {
